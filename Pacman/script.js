fc6638ee0fe7e3c3b5f671bf9468084a0435d239
Sparrow8104 <anchalsingh8102004@gmail.com>
2024-11-09 22:15:15 +0530

    changed square boxes to pipe images

--- Pacman/script.js
+++ Pacman/script.js
@@ -10,14 +10,16 @@ class Boundary {
     Boundary.width = 40
     Boundary.height = 40
   }
-  constructor({ position }) {
+  constructor({ position,image }) {
     this.position = position
     this.width = 40
     this.height = 40
+    this.image=image
   }
   draw() {
-    c.fillStyle = 'blue'
-    c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    // c.fillStyle = 'blue'
+    // c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    c.drawImage(this.image,this.position.x,this.position.y)
   }
 }
 
@@ -81,6 +83,8 @@ const map = [
   ['-', ' ', ' ', ' ', ' ', ' ', '-'],
   ['-', '-', '-', '-', '-', '-', '-'],
 ]
+const image=new Image()
+image.src='./img/pipeHorizontal.png'
 map.forEach((row, i) => {
   row.forEach((symbol, j) => {
     switch (symbol) {
@@ -89,7 +93,8 @@ map.forEach((row, i) => {
           position: {
             x: Boundary.width * j,
             y: Boundary.height * i
-          }
+          },
+          image:image
         })
       )
         break;
